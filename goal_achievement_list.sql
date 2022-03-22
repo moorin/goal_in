2ecec7162d3edf16539a13b68c297d5812d2b54a
moo <onpa@naver.com>
2022-03-22 15:29:30 +0900

    modified table

--- goal_achievement_list.sql
+++ goal_achievement_list.sql
@@ -1,7 +1,7 @@
 create table goal_achievement_list (
   num int not null auto_increment,
   name varchar(20) not null,
-  regist_day timestamp not null,
+  regist_day timestamp not null DEFAULT CURRENT_TIMESTAMP,
   target_date date not null,
   title varchar(200) not null,
   goal_achievement int not null,
--- test.sql
+++ test.sql
@@ -1,9 +1,9 @@
-insert into goal_achievement_list (name, regist_day, target_date, title, goal_achievement) values("hyerin", systimestamp, "2022-03-25", "read 3 books", 1);
-insert into goal_achievement_list (name, regist_day, target_date, title, goal_achievement) values("moo", systimestamp, "2022-03-11", "1 kg weight loss", 2);
-insert into goal_achievement_list (name, regist_day, target_date, title, goal_achievement) values("tom", systimestamp, "2021-04-25", "pass the exam", 2);
-insert into goal_achievement_list (name, regist_day, target_date, title, goal_achievement) values("grace", systimestamp, "2020-04-03", "project completion", 2);
-insert into goal_achievement_list (name, regist_day, target_date, title, goal_achievement) values("poo", systimestamp, "2020-06-15", "reach game rank 2400", 3);
-insert into goal_achievement_list (name, regist_day, target_date, title, goal_achievement) values("hoho", systimestamp, "2022-12-25", "make love", 1);
+insert into goal_achievement_list (name, target_date, title, goal_achievement) values("hyerin", "2022-03-25", "read 3 books", 1);
+insert into goal_achievement_list (name, target_date, title, goal_achievement) values("moo", "2022-03-11", "1 kg weight loss", 2);
+insert into goal_achievement_list (name, target_date, title, goal_achievement) values("tom", "2021-04-25", "pass the exam", 2);
+insert into goal_achievement_list (name, target_date, title, goal_achievement) values("grace", "2020-04-03", "project completion", 2);
+insert into goal_achievement_list (name, target_date, title, goal_achievement) values("poo", "2020-06-15", "reach game rank 2400", 3);
+insert into goal_achievement_list (name, target_date, title, goal_achievement) values("hoho", "2022-12-25", "make love", 1);
 
 update goal_achievement_list set title="modify title" where num=1;
 
